e19c178720ca4c40043ee83f4850bb1104dafa2f
abdr <asan.abdirakhym@gmail.com>
2023-08-13 13:37:49 +0600

    task

---
+++ VoxPop/app.py
@@ -0,0 +1,17 @@
+﻿from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Хранилище комментариев в виде списка словарей
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form["comment_text"]
+        category = request.form["comment_category"]
+        comments.append({"text": text, "category": category})
+    return render_template("index.html", comments=comments)
+
+if __name__ == "__main__":
+    app.run(debug=True)
